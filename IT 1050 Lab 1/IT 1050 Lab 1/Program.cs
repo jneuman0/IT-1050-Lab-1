f9ae72c242c4eaf4782556156e7bb8ab7250fdc4
jneuman0 <35542968+jneuman0@users.noreply.github.com>
2018-02-06 20:56:58 -0500

    imsmart
    imsmart

---
+++ IT 1050 Lab 1/IT 1050 Lab 1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace IT_1050_Lab_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            System.Console.Write("What is your first name?");
+            string firstName = System.Console.ReadLine();
+
+
+
+
+
+
+        }
+    }
+}
